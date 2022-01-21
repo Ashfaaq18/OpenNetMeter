489b71a024be087348d3046e9b9f9c9fe46a51ea
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2022-01-21 22:21:30 +0530

    Added minimize to tray base with open and exit, Minor code refactoring, Todo: Add a small popup to show usage stats.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using WhereIsMyData.ViewModels;
 using System.Windows.Input;
+using Forms = System.Windows.Forms;
+using System;
 
 namespace WhereIsMyData.Views
 {
@@ -10,13 +12,49 @@ namespace WhereIsMyData.Views
     public partial class MainWindow : Window
     {
         private AboutWindow aboutWin;
+        private Forms.NotifyIcon ni;
+        private Forms.ContextMenuStrip cm;
+        private Forms.MenuStrip ms;
         public MainWindow()
         {
             InitializeComponent();
+            
+            //initialize system tray
+            ni = new Forms.NotifyIcon();
+            cm = new Forms.ContextMenuStrip();
+            ms = new Forms.MenuStrip();
+
+            ni.Icon = new System.Drawing.Icon("Resources/myicon1.ico");
+            ni.Visible = true;
+            ni.DoubleClick += Ni_DoubleClick;
+            cm.Items.Add("Open", null, Cm_Open_Click);
+            cm.Items.Add("Exit", null, Cm_Exit_Click);
+
+            ni.ContextMenuStrip = cm;
+
             DataContext = new NavigationAndTasksVM();
             aboutWin = new AboutWindow();
         }
-        
+
+        private void Cm_Open_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Cm_Exit_Click(object sender, System.EventArgs e)
+        {
+            ms.Dispose();
+            cm.Dispose();
+            ni.Dispose();
+            aboutWin.Close();
+            this.Close();
+        }
+
+        private void Ni_DoubleClick(object sender, System.EventArgs e)
+        {
+            this.Show();
+        }
+
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
@@ -29,8 +67,8 @@ namespace WhereIsMyData.Views
         
         private void Exit_Button_Click(object sender, RoutedEventArgs e)
         {
-            aboutWin.CloseWin();
-            Close();
+            aboutWin.Hide();
+            this.Hide();
         }
 
         private void About_Button_Click(object sender, RoutedEventArgs e)
