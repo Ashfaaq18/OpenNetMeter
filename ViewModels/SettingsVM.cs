934fc9ac670cdfe2975b085d887a579eb3c2bf8e
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2022-01-30 17:09:34 +0530

    Added option for the application to startup on windows boot. Avaiable in the settings section.

--- ViewModels/SettingsVM.cs
+++ ViewModels/SettingsVM.cs
@@ -1,19 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using OpenNetMeter.Models;
+using System.Reflection;
+using Microsoft.Win32.TaskScheduler;
 
 namespace OpenNetMeter.ViewModels
 {
-    public class SettingsVM
+    public class SettingsVM : INotifyPropertyChanged
     {
-        public SettingsVM()
+        private bool setStartWithWin;
+        public bool SetStartWithWin
         {
+            get { return setStartWithWin; }
+
+            set
+            {
+                if (setStartWithWin != value)
+                {
+                    setStartWithWin = value;
+                    OnPropertyChanged("SetStartWithWin");
+
+                    //set the app settings
+                    Properties.Settings.Default.StartWithWin = value;
+                    Properties.Settings.Default.Save();
+
+                    UnlockOptionStartWin = false;
+                    //register to task scheduler
+                    SetAppAsTask(value);
+                    UnlockOptionStartWin = true;
+                    
+                }
+            }
+        }
+        
+        private bool unlockOptionStartWin;
+        public bool UnlockOptionStartWin
+        {
+            get { return unlockOptionStartWin; }
+
+            set
+            {
+                if (unlockOptionStartWin != value)
+                {
+                    unlockOptionStartWin = value;
+                    OnPropertyChanged("UnlockOptionStartWin");
+                }
+            }
         }
 
+
+        public SettingsVM()
+        {
+            taskFolder = "OpenNetMeter";
+            taskName = "OpenNetMeter" + "-" + Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+            UnlockOptionStartWin = true;
+            SetStartWithWin = Properties.Settings.Default.StartWithWin;
+        }
+
+        private readonly string taskName;
+        private readonly string taskFolder;
+        private void SetAppAsTask(bool set)
+        {
+            try
+            {
+                TaskFolder sub = TaskService.Instance.RootFolder.SubFolders["OpenNetMeter"];
+                if (!set)
+                {
+                    for(int i = 0; i < sub.Tasks.Count; i++)
+                    {
+                        sub.DeleteTask(sub.Tasks[i].Name);
+                    }
+
+                    TaskService.Instance.RootFolder.DeleteFolder(taskFolder);
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (set)
+                {
+                    try
+                    {
+                        TaskService.Instance.RootFolder.CreateFolder(taskFolder);
+                        CreateTask();
+                    }
+                    catch (Exception ex1)
+                    {
+                        Debug.WriteLine("Error: " + ex1.Message);
+                    }
+                }
+            }
+        }
+
+        private void CreateTask()
+        {
+            try
+            {
+                //create task
+                TaskDefinition td = TaskService.Instance.NewTask();
+                td.RegistrationInfo.Description = "Run OpenNetMeter on system log on";
+                // Set to run at the highest privilege
+                td.Principal.RunLevel = TaskRunLevel.Highest;
+                // Task only runs when user is logged on
+                td.Principal.LogonType = TaskLogonType.InteractiveToken;
+
+                // These settings will ensure it runs even if on battery power
+                td.Settings.DisallowStartIfOnBatteries = false;
+                td.Settings.StopIfGoingOnBatteries = false;
+
+                //set compatibility to windows 10
+                td.Settings.Compatibility = TaskCompatibility.V2_3;
+
+                //set to launch when user logs on
+                LogonTrigger logonTrigger = new LogonTrigger
+                {
+                    Enabled = true,
+                    UserId = null
+                };
+                td.Triggers.Add(logonTrigger);
+
+                //set action to run application
+                ExecAction action = new ExecAction();
+                action.Path = Path.Combine(AppContext.BaseDirectory, "OpenNetMeter.exe");
+                td.Actions.Add(action);
+
+                // Register the task in the sub folder
+                TaskService.Instance.RootFolder.SubFolders["OpenNetMeter"].RegisterTaskDefinition(taskName, td);
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        //------property changers---------------//
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
     }
 }
