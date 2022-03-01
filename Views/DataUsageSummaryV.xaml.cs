b365a0eb7c2f3555fe438a8b79aa9cb7aa0baacb
Ashfaaq Riphque <ashfaaq.riphque@gmail.com>
2022-03-01 19:34:11 +0530

    Revert "Can now resize the application. (#22)" (#24)
    This reverts commit f50027993d8475c3c2324bba7ae83da4fb9acf8d.

--- Views/DataUsageSummaryV.xaml.cs
+++ Views/DataUsageSummaryV.xaml.cs
@@ -19,6 +19,7 @@ namespace OpenNetMeter.Views
     public partial class DataUsageSummaryV : UserControl
     {
         private DispatcherTimer resizeTimer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 0, 0, 200), IsEnabled = false };
+        private bool isLoaded = false;
         private DataUsageSummaryVM dusvm;
 
         private List<TextBlock> Xlabels;
@@ -35,6 +36,7 @@ namespace OpenNetMeter.Views
             Loaded += delegate
             {
                 dusvm = (DataUsageSummaryVM)this.DataContext;
+                isLoaded = true;
 
                 Ylabels = new List<TextBlock>();
                 Xlabels = new List<TextBlock>();
@@ -114,7 +116,6 @@ namespace OpenNetMeter.Views
                 Graph.Children.Add(GridBorder);
 
                 resizeTimer.Tick += resizeTimer_Tick;
-                Graph_SizeChanged(null,null);
             };
         }
 
@@ -138,7 +139,7 @@ namespace OpenNetMeter.Views
         //scale the graph coordinates
         private void Graph_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
         {
-            if(dusvm != null)
+            if(isLoaded)
             {
                 resizeTimer.IsEnabled = true;
                 resizeTimer.Stop();
