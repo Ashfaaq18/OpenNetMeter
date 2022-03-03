7f63e204751c6b202fb68211cb71d10bacc8b21e
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2022-03-03 13:26:51 +0530

    Added exception handling to GetLocalIP()

--- Models/NetworkInfo.cs
+++ Models/NetworkInfo.cs
@@ -72,9 +72,18 @@ namespace OpenNetMeter.Models
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
             {
-                socket.Connect("8.8.8.8", 65530);
-                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-                return endPoint.Address.ToString();
+                try
+                {
+                    socket.Connect("8.8.8.8", 65530);
+                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                    return endPoint.Address.ToString();
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+
+                return "NoIP";
             }
         }
 
