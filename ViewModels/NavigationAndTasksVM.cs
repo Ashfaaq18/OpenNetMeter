bd3c662d08cac27dbbd2bf2a6b46f9958f53fb72
Ashfaaq18 <ashfaaq.riphque@gmail.com>
2022-03-07 15:50:30 +0530

    pass taskbar speed values only when taskbar display is enabled

--- ViewModels/NavigationAndTasksVM.cs
+++ ViewModels/NavigationAndTasksVM.cs
@@ -100,9 +100,12 @@ namespace OpenNetMeter.ViewModels
             tpvm.DownloadSpeed = netInfo.DownloadSpeed;
             tpvm.UploadSpeed = netInfo.UploadSpeed;
             //update speed in taskbar
-            (double,int) down = DataSizeSuffix.SizeSuffixInInt(netInfo.DownloadSpeed);
-            (double,int) up = DataSizeSuffix.SizeSuffixInInt(netInfo.UploadSpeed);
-            SettingsVM.SetDataVars(down.Item1, down.Item2, up.Item1, up.Item2);
+            if(Properties.Settings.Default.DeskBandSetting)
+            {
+                (double, int) down = DataSizeSuffix.SizeSuffixInInt(netInfo.DownloadSpeed);
+                (double, int) up = DataSizeSuffix.SizeSuffixInInt(netInfo.UploadSpeed);
+                SettingsVM.SetDataVars(down.Item1, down.Item2, up.Item1, up.Item2);
+            }
         }
 
         private void OpenDataUsageSum(object obj)
