1df556ce27768dd082a1c5eefbac29f048e9a1b9
Ash <ashfaaq.riphque@gmail.com>
2025-10-12 03:20:24 +0800

    add debug lines

--- OpenNetMeter/Utilities/EventLogger.cs
+++ OpenNetMeter/Utilities/EventLogger.cs
@@ -9,17 +9,27 @@ namespace OpenNetMeter.Utilities
         private const string LogName = "Application";
 
         public static void Info(string message, int eventId = 1000)
-            => EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Information, eventId);
+        {
+            Debug.WriteLine(message);
+            EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Information, eventId);
+        }
 
         public static void Warn(string message, int eventId = 2000)
-            => EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Warning, eventId);
+        {
+            Debug.WriteLine(message);
+            EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Warning, eventId);
+        }
 
         public static void Error(string message, int eventId = 3000, short category = 0)
-            => EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Error, eventId);
+        {
+            Debug.WriteLine(message);
+            EventLog.WriteEntry(EventSourceName, message, EventLogEntryType.Error, eventId);
+        }
 
         public static void Error(string message, Exception ex, int eventId = 3001)
         {
             string errorMessage = $"{message}{Environment.NewLine}{ex}";
+            Debug.WriteLine(errorMessage);
             EventLog.WriteEntry(EventSourceName, errorMessage, EventLogEntryType.Error, eventId);
         }
     }
