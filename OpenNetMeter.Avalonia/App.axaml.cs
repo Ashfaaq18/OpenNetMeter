ca5e410db22e9fbbac196070b69c6f59abb9d593
Ash <ashfaaq.riphque@gmail.com>
2026-03-06 18:46:10 +0800

    add a network capturing placeholder for linux. add custom window resizer to support linux custom window chrome

--- OpenNetMeter.Avalonia/App.axaml.cs
+++ OpenNetMeter.Avalonia/App.axaml.cs
@@ -1,9 +1,11 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using OpenNetMeter.Avalonia.Services;
 using OpenNetMeter.Avalonia.ViewModels;
 using OpenNetMeter.Avalonia.Views;
+using OpenNetMeter.PlatformAbstractions;
 
 namespace OpenNetMeter.Avalonia;
 
@@ -19,7 +21,9 @@ public partial class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             var windowService = new AvaloniaWindowService();
-            var networkCaptureService = new WindowsNetworkCaptureService();
+            INetworkCaptureService networkCaptureService = OperatingSystem.IsWindows()
+                ? new WindowsNetworkCaptureService()
+                : new PlaceholderNetworkCaptureService();
             desktop.MainWindow = new MainWindow
             {
                 DataContext = new MainWindowViewModel(windowService, networkCaptureService)
--- OpenNetMeter.Avalonia/Views/MainWindow.axaml.cs
+++ OpenNetMeter.Avalonia/Views/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Controls.Primitives;
 using OpenNetMeter.Avalonia.ViewModels;
 
 namespace OpenNetMeter.Avalonia.Views;
@@ -14,6 +15,21 @@ public partial class MainWindow : Window
     private void TitleBar_PointerPressed(object? sender, PointerPressedEventArgs e)
     {
         BeginMoveDrag(e);
+    }
+
+    private void ResizeTop_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.North, e);
+    private void ResizeBottom_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.South, e);
+    private void ResizeLeft_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.West, e);
+    private void ResizeRight_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.East, e);
+    private void ResizeTopLeft_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.NorthWest, e);
+    private void ResizeTopRight_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.NorthEast, e);
+    private void ResizeBottomLeft_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.SouthWest, e);
+    private void ResizeBottomRight_PointerPressed(object? sender, PointerPressedEventArgs e) => TryBeginResize(WindowEdge.SouthEast, e);
+
+    private void TryBeginResize(WindowEdge edge, PointerPressedEventArgs e)
+    {
+        if (WindowState == WindowState.Normal)
+            BeginResizeDrag(edge, e);
     }
 
     protected override void OnClosed(System.EventArgs e)
